4abcf7ef867f959c9db147d346f3a36f6f5ba0a7
Zoran-Petrov <zoran_782002Qyahoo.com>
2019-01-19 13:47:51 +0200

    start write getUserDetails method

--- src/VacationsSystem.java
+++ src/VacationsSystem.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class VacationsSystem {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        printUserOptions();
+        System.out.println("Въведи избор:");
+        int userChoice = input.nextInt();
+        switch (userChoice) {
+            case 1:
+                getUserDetails();
+                break;
+
+        }
+    }
+    public static void printUserOptions() {
+        System.out.println("-------------------------------------------");
+        System.out.println("  1. Заяви отпуска.");
+        System.out.println("  2. Виж всички отпуски.");
+        System.out.println("  3. Виж отпуска за служител.");
+        System.out.println("  4. Промени статус на отпуска");
+        System.out.println("  5. Изход.");
+        System.out.println("-------------------------------------------");
+    }
+
+    public static void getUserDetails() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Моля въведете собствено име");
+        String firstName = input.nextLine();
+        System.out.println(firstName);
+
+
     }
 }
