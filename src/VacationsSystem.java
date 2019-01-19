7680b83f3352d2248229b5ff55a5691e44fdcd20
Zoran-Petrov <zoran_782002Qyahoo.com>
2019-01-19 12:51:26 +0200

    user options method added

--- src/VacationsSystem.java
+++ src/VacationsSystem.java
@@ -1,5 +1,15 @@
 public class VacationsSystem {
     public static void main(String[] args) {
-        
+        printUserOptions();
     }
+    public static void printUserOptions() {
+        System.out.println("-------------------------------------------");
+        System.out.println("  1. Заяви отпуска.");
+        System.out.println("  2. Виж всички отпуски.");
+        System.out.println("  3. Виж отпуска за служител.");
+        System.out.println("  4. Промени статус на отпуска");
+        System.out.println("  5. Изход.");
+        System.out.println("-------------------------------------------");
+    }
+
 }
