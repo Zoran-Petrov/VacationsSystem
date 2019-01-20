cad14a89b7d607cd0fdcdca2936ab62c96951e85
Zoran-Petrov <zoran_782002Qyahoo.com>
2019-01-20 14:17:39 +0200

    creates getVacationPeriod()

--- src/VacationsSystem.java
+++ src/VacationsSystem.java
@@ -148,5 +148,37 @@ public class VacationsSystem {
         }
     }
 
+    public static void getUserId() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Моля въведете ЕГН:");
+        String id = input.nextLine();
+        if (id == null || id.isEmpty()) {
+            System.out.println("Моля въведете ЕГН!");
+            id = input.nextLine();
+        } else {
+            while (!id.matches("[0-9]{10}")) {
+                System.out.println("Моля въведете валиден ЕГН!");
+                id = input.nextLine();
+            }
+        }
+    }
+
+    public static void getVacationPeriod() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Моля въведете начална и крайна дата в формат DD/MM/2019-DD/MM/2019:");
+        String vacationPeriod = input.nextLine();
+        if (vacationPeriod == null || vacationPeriod.isEmpty()) {
+            System.out.println("Моля въведете начална и крайна дата!");
+            vacationPeriod = input.nextLine();
+
+        } else {
+            String dateRegex = "[0-9]{2}/[0-9]{2}/(2019)-[0-9]{2}/[0-9]{2}/(2019)";
+            while (!vacationPeriod.matches(dateRegex)) {
+                System.out.println("Моля въведете начална и крайна дата в указаният формат!");
+                vacationPeriod = input.nextLine();
+            }
+        }
+    }
+
 }
 
